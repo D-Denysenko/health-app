0299e504ec2514e9258b8571d7a9c8ffbb0b0113
D-Denysenko <d.denysenko98@gmail.com>
2021-03-22 18:22:35 +0200

    changes for pull requests

--- mood_sense/tests.py
+++ mood_sense/tests.py
@@ -9,7 +9,7 @@ class ValidateCoordinatesTest(TestCase):
     def setUp(self):
         User = get_user_model()
         user = User.objects.create_user('danil2', 'freeforse@outlook.com', 'd82485761')
-        self.client.login(username='danil2', password='d82485761')
+        self.client.login(username='change_login', password='change_password')
 
     def test_upload_photo_without_coordinates(self):
         with open(join(MEDIA_ROOT, 'mood', '2019', '10', '25', '2.jpeg'), 'rb') as file:
@@ -32,3 +32,14 @@ class AuthTest(TestCase):
     def test_secure_page(self):
         response = self.client.post('/api/v1/mood/upload', follow=True)
         self.assertEqual(response.status_code, 403)
+
+
+class ExampleTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create_user('aaaa', 'freeforse@outlook.com', 'd82485sss761')
+
+    def test_secure_page(self):
+        response = self.client.post('/api/v1/mood/upload', follow=True)
+        self.assertEqual(response.status_code, 403)
+
